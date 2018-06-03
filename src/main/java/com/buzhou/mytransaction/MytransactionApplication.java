9d6c54c49947b4298cfeb1275bc329df47f655a7
mutouji <59022665@qq.com>
2018-06-03 10:39:06 +0800

    first commit

---
+++ src/main/java/com/buzhou/mytransaction/MytransactionApplication.java
@@ -0,0 +1,12 @@
+package com.buzhou.mytransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytransactionApplication.class, args);
+	}
+}
